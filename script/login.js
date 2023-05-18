8cb708395e176bd12c411573c7581c0503762755
Teo <120208346+teo-101@users.noreply.github.com>
2023-05-18 12:05:19 +0300

    Update login.js

--- script/login.js
+++ script/login.js
@@ -3,21 +3,16 @@ const form = document.getElementById('loginForm');
         const password = document.getElementById('password');
         const button = document.getElementById('formBtn');
 
-        function submitForm(event) {
-            let user = username.value;
-            let pass = password.value;
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            let user = username.value.trim();
+            let pass = password.value.trim();
         
-            if (user === '' || pass === '') {
+            if (user !== '' && pass !== '') {
+                window.location.href = 'index.html';
+            } else {
                 username.classList.add('error');
                 password.classList.add('error');
                 button.classList.add('error');
-                event.preventDefault();
-            } else {
-                window.location.href = '../index_main_page.html';
-                username.classList.remove('error');
-                password.classList.remove('error');
-                button.classList.remove('error');
             }
-        }
-
-        form.addEventListener('submit', submitForm);
+        });
