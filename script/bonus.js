629afe142801673a19740f364b926a58465b5a89
Teo <th3od0ry@gmail.com>
2023-05-20 01:02:49 +0300

    ready

--- script/bonus.js
+++ script/bonus.js
@@ -1,13 +1,27 @@
-const input = document.querySelector('.terminalInput');
+const inputs = Array.from(document.querySelectorAll('.terminalInput'));
 const button = document.getElementById('testButton');
 
 button.addEventListener('click', (event) => {
 
-  if (input.value === "") {
-    console.log('works');
+  if (inputs[0].value === 'ngrok service start') {
+    inputs[0].classList.add('corect');
+  } else {
+    inputs[0].classList.add('gresit');
   }
-  else {
-    console.log('oops');
+  if (inputs[1].value === 'target:@i.am_delete -followers') {
+    inputs[1].classList.add('corect');
+  } else {
+    inputs[1].classList.add('gresit');
+  }
+  if (inputs[2].value === 'target:@i.am_delete -fwingsemail') {
+    inputs[2].classList.add('corect');
+  } else {
+    inputs[2].classList.add('gresit');
+  }
+  if (inputs[3].value === 'target:@i.am_delete -info') {
+    inputs[3].classList.add('corect');
+  } else {
+    inputs[3].classList.add('gresit');
   }
 });
 
