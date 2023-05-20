ea3331b8dafffdc1a7e02e587c3679b3b8783841
IamDelete <132224496+IamDelete@users.noreply.github.com>
2023-05-20 09:10:06 +0300

    Update bonus.js

--- script/bonus.js
+++ script/bonus.js
@@ -13,12 +13,12 @@ button.addEventListener('click', (event) => {
   } else {
     inputs[1].classList.add('gresit');
   }
-  if (inputs[2].value === 'target:@i.am_delete -fwingsemail') {
+  if (inputs[2].value === 'target:@i_am.delete -fwingsemail') {
     inputs[2].classList.add('corect');
   } else {
     inputs[2].classList.add('gresit');
   }
-  if (inputs[3].value === 'target:@i.am_delete -info') {
+  if (inputs[3].value === 'target:@i_am,delete -info') {
     inputs[3].classList.add('corect');
   } else {
     inputs[3].classList.add('gresit');
